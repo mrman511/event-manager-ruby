c37765874de35b5bdf52df917e88f5a804487250
Paul Bodner <bodner80@gmail.com>
2024-08-15 14:37:17 -0700

    Created todo model. Created todo controller at /api/v1,

---
+++ app/controllers/api/v1/todos_controller.rb
@@ -0,0 +1,49 @@
+class Api::V1::TodosController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @todos = Todo.all
+    render json: @todos
+  end
+
+  def show
+    @todo = Todo.find(params[:id])
+    render json: @todos
+  end
+
+  def create 
+    @todo = Todo.new(valid_params)
+    if @todo.save
+      render json: @todo, status: 200
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @todo = Todo.find(params[:id])
+    if @todo
+      @todo.update!(valid_params)
+      render json: {message: 'Todo updated'}, status: 200
+    else
+      render json: {error: 'unable to update Todo'}, status: 500
+    end
+  end
+  
+  def destroy
+    @todo = Todo.find(params[:id])
+    if @todo
+      @todo.destroy
+      render json: {message: 'Todo destroyed'}, status: 200
+    else
+      render json: {error: 'unable to destroy Todo'}, status: 500
+    end
+  end
+
+  private 
+
+  def valid_params
+    params.require(:todo).permit(:title, :status, :is_completed)
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,5 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_14_225621) do
+  create_table "todos", force: :cascade do |t|
+    t.string "title"
+    t.string "status"
+    t.boolean "is_completed"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 end
