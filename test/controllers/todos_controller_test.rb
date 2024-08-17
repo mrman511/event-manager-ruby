076bcfcdc139262677823f75c3ca478e9ef95d64
Paul Bodner <bodner80@gmail.com>
2024-08-17 10:14:59 -0700

    fix merge errors caused by fixing errors from issue 4 on branch 5-create-tests...

--- test/controllers/todos_controller_test.rb
+++ test/controllers/todos_controller_test.rb
@@ -1,9 +1,7 @@
 require "test_helper"
 
 class TodosControllerTest < ActionDispatch::IntegrationTest
-  test "Retrieves Index page" do
-    get '/api/v1/todos'
-    assert_response :success
-    assert_not_nil assigns(:todos)
-  end
+  # test "the truth" do
+  #   assert true
+  # end
 end
