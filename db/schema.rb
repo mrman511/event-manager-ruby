e54611028812f0f4689f6619bb887ba29287dffa
Paul Bodner <bodner80@gmail.com>
2024-08-19 18:36:45 -0700

    created and migrated user model which has one unique key 'email'

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_14_225621) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_19_232131) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.string "status"
     t.boolean "is_completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
