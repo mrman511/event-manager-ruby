b9981dbef516bc6b6ffcce5f7967bf32d08869d9
Paul Bodner <bodner80@gmail.com>
2024-08-19 19:04:51 -0700

    Updated User models to reflect migration, Updated user with password and confrimation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_08_19_232131) do
+ActiveRecord::Schema[7.2].define(version: 2024_08_20_015837) do
   create_table "todos", force: :cascade do |t|
     t.string "title"
     t.string "status"
@@ -23,6 +23,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_08_19_232131) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.datetime "confirmed_at"
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 end
