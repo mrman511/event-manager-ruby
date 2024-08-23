72516e6141171ed26673a314ca27314ff83d4e8c
Fred Ulrich <9284621+fulrich@users.noreply.github.com>
2024-08-22 21:47:56 -0400

    Adding the Todo model

--- test/models/todo_test.rb
+++ test/models/todo_test.rb
@@ -1,7 +1,39 @@
 require "test_helper"
 
 class TodoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @valid_attributes = {
+      title: "An excellent todo",
+      status: "created",
+      is_completed: false
+    }
+  end
+
+  test "#create! creates a todo that can be fetched from the database" do
+    created_todo = Todo.create!(@valid_attributes)
+    fetched_todo = Todo.find(created_todo.id)
+
+    assert_equal @valid_attributes[:title], fetched_todo.title
+    assert_equal @valid_attributes[:status], fetched_todo.status
+    assert_equal @valid_attributes[:is_completed], fetched_todo.is_completed
+  end
+
+  test "#title is required to create a new todo" do
+    assert_raises { Todo.create!(@valid_attributes.delete(:title)) }
+  end
+
+  test "#title cannot create a todo with a title over 25 characters" do
+    extra_long_title = "A" * 26
+    @valid_attributes[:title] = extra_long_title
+
+    assert_raises { Todo.create!(@valid_attributes) }
+  end
+
+  test "#status is required to create a new todo" do
+    assert_raises { Todo.create!(@valid_attributes.delete(:status)) }
+  end
+
+  test "#is_completed is required to create a new todo" do
+    assert_raises { Todo.create!(@valid_attributes.delete(:is_completed)) }
+  end
 end
