c90969e9643606d7b58005ad4e857e9079f4deb9
Paul Bodner <bodner80@gmail.com>
2024-08-23 23:41:59 -0700

    created todo controller for fetch (multiple and single) and create

---
+++ app/controllers/todos_controller.rb
@@ -0,0 +1,26 @@
+class TodosController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    render json: Todo.all
+  end
+
+  def show
+    render json: Todo.find(params[:id])
+  end
+
+  def create
+    todo = Todo.create(permitted_params)
+    if todo.valid?
+      render json: todo, status: :accepted
+    else
+      render json: todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def permitted_params
+    params.require(:todo).permit(:title, :status, :is_completed)
+  end
+end
