1758613e6a44df94a591dffbb9d45d01b6740cf3
Paul Bodner <bodner80@gmail.com>
2024-08-29 09:24:05 -0700

    fixed linting errors cause by previous commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,9 +20,9 @@ class User < ApplicationRecord
       " must contain at least one digit"             => /\d+/,
       " must contain at least one special character" => /[^A-Za-z0-9]+/
     }
-  
+
     rules.each do |message, regex|
-      errors.add( :password, message ) unless password.match( regex )
+      errors.add(:password, message) unless password.match(regex)
     end
   end
 end
