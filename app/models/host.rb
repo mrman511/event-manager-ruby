b163d3978624cb46665a7ed21a5a3e3c03044d4f
Paul Bodner <bodner80@gmail.com>
2024-10-14 22:10:31 -0700

    Created host model

--- app/models/host.rb
+++ app/models/host.rb
@@ -1,28 +1,57 @@
 class Host < ApplicationRecord
   has_many :events
 
-  # def new_event_start_conflict(new_event, event){
-  #   # test
-  #   if event.ends
-  #     ends = event.ends
-  #     # test
-  #   else
-  #     # test
-  #     ends = event.starts + 24.hours
-  #   end
+  def create_event(event_params)
+    if event_params
+      event_params["host"] = self
+      new_event = Event.new(event_params)
+      validate_created_event(new_event)
+      add_event(new_event)
+      return new_event
+    end
+    raise Exception.new "Event params are required"
+  end
 
-  #   if new_event.starts >= event.starts && new_event.
-  # }
-  
+  def delete_event(event_id)
+    if !event_id
+      raise Exception.new "No event requested for removal"
+    end
+    self.events.each do |comparison_event|
+      if comparison_event.id === event_id
+        comparison_event.destroy()
+        return
+      end
+    end
+    raise Exception.new "Event not found"
+  end
+
+  private
+
   def add_event(new_event)
-    if new_event
-      if self.events == nil
-        self.events = [new_event]
-      end
-      if self.events
-        self.events.push(new_event)
-      end
-      self.save
+    if self.events.count === 0
+      self.events = [ new_event ]
+    else
+      self.events.push(new_event)
+    end
+  end
+
+  def validate_created_event(new_event)
+    self.events.each do |comparison_event|
+      validate_new_event_starts_time(new_event, comparison_event)
+      validate_current_event_start_times(new_event, comparison_event)
+    end
+    new_event.save!
+  end
+
+  def validate_new_event_starts_time(new_event, comparison_event)
+    if new_event.starts >= comparison_event.starts && new_event.starts < comparison_event.ends
+      raise Exception.new "Added event conflicts with another hosted Event"
+    end
+  end
+
+  def validate_current_event_start_times(new_event, comparison_event)
+    if comparison_event.starts >= new_event.starts && comparison_event.starts < new_event.ends
+      raise Exception.new "Added event conflicts with another hosted Event"
     end
   end
 end
--- test/models/host_test.rb
+++ test/models/host_test.rb
@@ -9,7 +9,7 @@ class HostTest < ActiveSupport::TestCase
       description: "Champions, welcome! The stars have aligned! The festival is nigh! General Radahn, mightiest demigod of the Shattering, awaits you! A celebration of war! The Radahn Festival!",
       postscript: "Are you good and prepared, young chum? The festival begins!",
       starts: DateTime.now() + 1.hours,
-      ends: DateTime.now() + 4.hours,
+      ends: DateTime.now() + 4.hours
     }
 
     @valid_event_params = {
@@ -19,27 +19,31 @@ class HostTest < ActiveSupport::TestCase
       description: "You must abide alone a while. Welcome, honoured guest. To the birthplace of our dynasty! Miquella is mine and mine alone.",
       postscript: "Praise the formless mother",
       starts: DateTime.now() + 1.hours,
-      ends: DateTime.now() + 4.hours,
+      ends: DateTime.now() + 4.hours
     }
 
     @valid_host_params = {
       name: "Mohgwyn Dynasty"
     }
 
+    @base_host = Host.create({ name: "Redmanes" })
     @base_event = Event.create({
+      host: @base_host,
       title: "Kill the Graven Witch",
       tagline: "We must defeat Sellen",
       location: "Raya Lucaria Library",
       description: "Sellen, Graven Witch, enemy of Caria. I vow this time to crush both your frame and your primal glintstone. I am Jerren, bringer of your death. Do not forget.",
-      starts: DateTime.now() + 1.hours,
-      ends: DateTime.now() + 4.hours,
+      starts: DateTime.now() + 36.hours,
+      ends: DateTime.now() + 40.hours
     })
-    @base_host = Host.create({ name: "Redmanes" })
-    @base_host.add_event(@base_event)
   end
-  
+
+  # ##################
+  # ##### CREATE #####
+  # ##################
+
   test "#create creates a host with valid params without an event" do
-    assert_difference("Host.count"){
+    assert_difference("Host.count") {
       Host.create!(@valid_host_params)
     }
   end
@@ -49,19 +53,81 @@ class HostTest < ActiveSupport::TestCase
     fetched_host = Host.find(created_host.id)
     assert_equal created_host.name, fetched_host.name
   end
-  
-  test "#add_event adds an event to a hosts events when a host with no events adds an event" do
+
+  # ##################
+  # ## CREATE_EVENT ##
+  # ##################
+
+  test "#create_event creates a new event with valid_params" do
+    assert_difference("Event.count") do
+      @base_host.create_event(@base_host_event_params)
+    end
+  end
+
+  test "#create_event adds an event to a hosts events when a host with no events adds an event" do
     created_host = Host.create(@valid_host_params)
-    new_event = Event.create(@valid_event_params)
-    assert_equal created_host.events.length, 0
-    created_host.add_event(new_event)
-    assert_equal created_host.events[0].id, new_event.id
+    assert_difference("created_host.events.length") do
+      created_host.create_event(@valid_event_params)
+    end
+  end
+
+  test "#create_event returns an event with valid params with the host called on as the host" do
+    created_host = Host.create(@valid_host_params)
+    created_event = created_host.create_event(@valid_event_params)
+    assert_equal @valid_event_params[:title], created_event.title
+    assert_equal @valid_event_params[:starts], created_event.starts
+    assert_equal @valid_event_params[:ends], created_event.ends
+    assert_equal created_host, created_event.host
+    assert created_event.instance_of?(Event)
   end
 
   test "#add_event adds an event to a hosts events when a host already has one or more events" do
     assert @base_host.events.length > 0
-    new_event = Event.create(@base_host_event_params)
-    @base_host.add_event(new_event)
-    assert true if @base_host.events.any? { |event| event.id == new_event.id}
+    new_event = @base_host.create_event(@base_host_event_params)
+    assert true if @base_host.events.any? { |event| event.id == new_event.id }
+  end
+
+  test "#add_event raises Exception if host has adds event that starts during another hosted event" do
+    @base_host.create_event(@base_host_event_params)
+    @base_host_event_params[:starts] += 4.minutes
+    @base_host_event_params[:ends] += 4.minutes
+    assert_raises(Exception) { @base_host.create_event(@base_host_event_params) }
+  end
+
+  test "#add_event raises Exception if host is already hosting an event that occurs during the new event" do
+    @base_host.create_event(@base_host_event_params)
+    @base_host_event_params[:starts] -= 4.minutes
+    @base_host_event_params[:ends] += 4.minutes
+    assert_raises(Exception) { @base_host.create_event(@base_host_event_params) }
+  end
+
+  test "#add_event raises Exception with no params provided" do
+    assert_raises(Exception) { @base_host.create_event() }
+  end
+
+  # ##################
+  # ## DELETE_EVENT ##
+  # ##################
+
+  test "#delete_event deletes an event with valid hosted event id" do
+    created_event = @base_host.create_event(@base_host_event_params)
+    assert_difference("Event.count", -1) do
+      @base_host.delete_event(created_event.id)
+    end
+  end
+
+  test "#delete_event removes event from host events with valid hosted event id" do
+    created_event = @base_host.create_event(@base_host_event_params)
+    assert_difference("@base_host.events.count", -1) do
+      @base_host.delete_event(created_event.id)
+    end
+  end
+
+  test "#delete_event raises Exception with no params provided" do
+    assert_raises(Exception) { @base_host.delete_event() }
+  end
+
+  test "#delete_event raises Exception with invalid params provided" do
+    assert_raises(Exception) { @base_host.delete_event(5) }
   end
 end
