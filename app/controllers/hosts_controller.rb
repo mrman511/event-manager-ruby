9bbb46482dbcb74b7daef40ab5f7fc4224d139d7
Paul Bodner <bodner80@gmail.com>
2024-10-19 22:48:54 -0700

    Created routes update and destroy for Host Controller

--- app/controllers/hosts_controller.rb
+++ app/controllers/hosts_controller.rb
@@ -17,7 +17,42 @@ class HostsController < ApplicationController
     end
   end
 
+  def update
+    host = Host.find(params[:id])
+    if host
+      if has_update(host)
+        host.update!(permitted_params)
+        render json: host, status: :accepted
+      else
+        render json: host, status: :not_modified
+      end
+    else
+      render json: host.errors, status: :not_found
+    end
+  end
+
+  def destroy
+    host = Host.find(params[:id])
+    if host
+      host.destroy
+      render json: { message: "host destroyed" }, status: :ok
+    else
+      render json: host.errors, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def has_update(host)
+    if !permitted_params.empty?
+      permitted_params.each do |key, value|
+        if value != host[key]
+          return true
+        end
+      end
+    end
+    false
+  end
 
   def permitted_params
     params.permit(:name)
